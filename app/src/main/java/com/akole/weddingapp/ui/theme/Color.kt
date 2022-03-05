e6a09267fdd0f5ebcb80a8bf1ab06dd0265fc498
Javier Camarena <javier.camtri@gmail.com>
2022-03-05 16:17:39 +0100

    Add color palletes

--- app/src/main/java/com/akole/weddingapp/ui/theme/Color.kt
+++ app/src/main/java/com/akole/weddingapp/ui/theme/Color.kt
@@ -6,3 +6,7 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Pink = Color(0xfff8bbd0)
+val LightPink = Color(0xffffeeff)
+val DarkPink = Color(0xffc48b9f)
