5feefc5316a9789d0852c41ff592038859b5684e
Javier Camarena <javier.camtri@gmail.com>
2022-03-19 11:46:12 +0100

    Fix issues

--- app/src/main/java/com/akole/weddingapp/ui/screens/splash/SplashScreenContent.kt
+++ app/src/main/java/com/akole/weddingapp/ui/screens/splash/SplashScreenContent.kt
@@ -1,10 +1,12 @@
 package com.akole.weddingapp.ui.screens.splash
 
 import androidx.compose.animation.core.*
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.animation.core.R
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -13,12 +15,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import com.akole.weddingapp.ui.theme.DarkPink
 
 @Composable
 fun SplashScreenContent(
@@ -27,8 +33,8 @@ fun SplashScreenContent(
     val heartTransition = rememberInfiniteTransition()
 
     val volumeHeart by heartTransition.animateFloat(
-        initialValue = 80f,
-        targetValue = 120f,
+        initialValue = 40f,
+        targetValue = 60f,
         animationSpec = infiniteRepeatable(
             animation = tween(
                 durationMillis = 800,
@@ -39,18 +45,20 @@ fun SplashScreenContent(
         )
     )
     Box(
-        modifier.fillMaxSize(),
+        modifier
+            .fillMaxSize()
+            .background(Color.White),
         contentAlignment = Alignment.Center
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "Boda\nLeyre & Javier",
-                fontSize = 40.sp,
-                fontWeight = FontWeight.ExtraBold,
-                fontFamily = FontFamily.Cursive,
-                textAlign = TextAlign.Center
+            val painter = rememberAsyncImagePainter(com.akole.weddingapp.R.drawable.gp_graphic)
+            Image(
+                painter = painter,
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.width(300.dp)
             )
             Icon(
                 imageVector = Icons.Filled.Favorite,
--- app/src/main/java/com/akole/weddingapp/ui/theme/Type.kt
+++ app/src/main/java/com/akole/weddingapp/ui/theme/Type.kt
@@ -9,14 +9,14 @@ import androidx.compose.ui.unit.sp
 import com.akole.weddingapp.R
 
 val fonts = FontFamily(
-        Font(R.font.league_spartan_black),
-        Font(R.font.league_spartan_bold, weight = FontWeight.Bold),
-        Font(R.font.league_spartan_light, weight = FontWeight.Light),
-        Font(R.font.league_spartan_thin, weight = FontWeight.Thin),
-        Font(R.font.league_spartan_extrabold, weight = FontWeight.ExtraBold),
-        Font(R.font.league_spartan_extralight, weight = FontWeight.ExtraLight),
-        Font(R.font.league_spartan_medium, weight = FontWeight.Medium),
-        Font(R.font.league_spartan_semibold, weight = FontWeight.SemiBold)
+        Font(R.font.elmessiri_regular),
+        Font(R.font.elmessiri_bold, weight = FontWeight.Bold),
+        Font(R.font.elmessiri_regular, weight = FontWeight.Light),
+        Font(R.font.elmessiri_regular, weight = FontWeight.Thin),
+        Font(R.font.elmessiri_bold, weight = FontWeight.ExtraBold),
+        Font(R.font.elmessiri_regular, weight = FontWeight.ExtraLight),
+        Font(R.font.elmessiri_medium, weight = FontWeight.Medium),
+        Font(R.font.elmessiri_semibold, weight = FontWeight.SemiBold)
 )
 
 // Set of Material typography styles to start with
@@ -25,17 +25,16 @@ val Typography = Typography(
                 fontFamily = fonts,
                 fontWeight = FontWeight.Normal,
                 fontSize = 16.sp
-        )
-        /* Other default text styles to override
+        ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.W500,
         fontSize = 14.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     )
-    */
+
 )
