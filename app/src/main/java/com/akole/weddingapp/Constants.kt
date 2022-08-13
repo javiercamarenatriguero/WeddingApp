07b277989d2ad8bfb56c3acb8714aaf87ddd05ee
Javier Camarena <javier.camtri@gmail.com>
2022-08-13 10:39:41 +0200

    Move Map Locations to Constants

--- app/src/main/java/com/akole/weddingapp/Constants.kt
+++ app/src/main/java/com/akole/weddingapp/Constants.kt
@@ -1,5 +1,8 @@
 package com.akole.weddingapp
 
+import com.akole.weddingapp.ui.screens.map.LocationUIItem
+import com.google.android.gms.maps.model.LatLng
+
 object Constants {
     // Random date
     internal const val WEDDING_TIMESTAMP = 1708367000000
@@ -9,4 +12,28 @@ object Constants {
     internal const val PHOTO_AVAILABLE_TIMESTAMP = 0
     // Wedding date string format
     internal const val DATETIME_FORMAT = "HH'h'mm - dd.MM.YYYY"
+    // Map Locations
+    // TODO: Inject by DI
+    internal val councilMarker = LocationUIItem(
+        position = LatLng(43.2640404644175, -2.9231387425674176),
+        title = R.string.council_title,
+        snippet = R.string.council_snippet,
+        image = R.drawable.marker1,
+        details = R.string.council_details,
+    )
+    internal val cocktelMarker = LocationUIItem(
+        position = LatLng(43.263880486320836, -2.9260678485581844),
+        title = R.string.cocktel_title,
+        snippet = R.string.cocktel_snippet,
+        image = R.drawable.marker2,
+        details = R.string.cocktel_details,
+
+        )
+    internal val restaurantMarker = LocationUIItem(
+        position = LatLng(43.29085987514852, -2.925274743006676),
+        title = R.string.restaurant_title,
+        snippet = R.string.restaurant_snippet,
+        image = R.drawable.marker3,
+        details = R.string.restaurant_details,
+    )
 }
--- app/src/main/java/com/akole/weddingapp/ui/screens/map/MapViewModel.kt
+++ app/src/main/java/com/akole/weddingapp/ui/screens/map/MapViewModel.kt
@@ -4,8 +4,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.akole.weddingapp.R
-import com.google.android.gms.maps.model.LatLng
+import com.akole.weddingapp.Constants.cocktelMarker
+import com.akole.weddingapp.Constants.councilMarker
+import com.akole.weddingapp.Constants.restaurantMarker
 
 class MapViewModel: ViewModel() {
 
@@ -34,33 +35,6 @@ class MapViewModel: ViewModel() {
     ) {
         state = UiState(
             mapItemPosition
-        )
-    }
-
-    companion object {
-        // TODO: Move those constants to a better package or inject by DI
-        // Location Items defined as constants
-        private val councilMarker = LocationUIItem(
-            position = LatLng(43.2640404644175, -2.9231387425674176),
-            title = R.string.council_title,
-            snippet = R.string.council_snippet,
-            image = R.drawable.marker1,
-            details = R.string.council_details,
-        )
-        private val cocktelMarker = LocationUIItem(
-            position = LatLng(43.263880486320836, -2.9260678485581844),
-            title = R.string.cocktel_title,
-            snippet = R.string.cocktel_snippet,
-            image = R.drawable.marker2,
-            details = R.string.cocktel_details,
-
-        )
-        private val restaurantMarker = LocationUIItem(
-            position = LatLng(43.29085987514852, -2.925274743006676),
-            title = R.string.restaurant_title,
-            snippet = R.string.restaurant_snippet,
-            image = R.drawable.marker3,
-            details = R.string.restaurant_details,
         )
     }
 }
